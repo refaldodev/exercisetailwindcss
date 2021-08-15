5a6b6085776a859cf90430d5cfc42ec945467a9e
refaldodev <m.refaldorachman@gmail.com>
2021-08-16 00:37:15 +0700

    first commit

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+let burger = document.querySelector('#burger');
+
+burger.addEventListener('click', function(){
+    alert('Hai my gorgeous friend on the internet 👋');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container:{
+      center: true,
+    padding: '1rem',
+  } ,
+  height: {
+    sm: '150px',
+    md: '400px',
+    lg: '400px',
+    xl: '400px',
+   },
+ 
+   fontSize: {
+    sm: ['12px', '20px'],
+    base: ['14px', '24px'],
+    lg: ['14px', '28px'],
+    xl: ['24px', '32px'],
+   }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
